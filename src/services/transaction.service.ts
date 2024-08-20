d4615dff27c5b92cae6c4df63946ff11a823f0be
Anang Adi <anang.adi182@gmail.com>
2024-08-20 10:51:26 +0700

    booking success, dashboard transaction

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,15 +1,39 @@
 import { apiSlice } from "./base-query";
 
 export const transactionApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    checkAvailableity: builder.mutation({
+  endpoints: (build) => ({
+    checkAvailability: build.mutation({
       query: (payload) => ({
         url: "/transaction/is-available",
         method: "POST",
         body: payload,
       }),
     }),
+    transaction: build.mutation({
+      query: (payload) => ({
+        url: "/transaction",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+    getTransactions: build.query({
+      query: () => ({
+        url: "/transaction",
+        method: "GET",
+      }),
+    }),
+    getDetailTransaction: build.query({
+      query: (id) => ({
+        url: `/transaction/${id}`,
+        method: "GET",
+      }),
+    }),
   }),
 });
 
-export const { useCheckAvailableityMutation } = transactionApi;
+export const {
+  useCheckAvailabilityMutation,
+  useTransactionMutation,
+  useGetTransactionsQuery,
+  useGetDetailTransactionQuery,
+} = transactionApi;
